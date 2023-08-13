039b995c58a0fd5d747bea6e64e4cf353fc3092e
MUCAHID KESKIN <141457652+KESKIN81@users.noreply.github.com>
2023-08-13 17:47:02 +0200

    Update IlkBranchIcin.java

--- IlkBranchIcin.java
+++ IlkBranchIcin.java
@@ -3,4 +3,5 @@ public class IlkBranchIcin {
     //bu benim ilk commitim olucak..
 
     // terminal ile yapilan ikinci commit ..
+    // main branch te githup uzerinden degisilik yapildi local`den degil 
 }
